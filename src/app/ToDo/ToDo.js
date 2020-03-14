73682396438c928c9f856ec5161549eb8e645675
Jeunn Hao Chong <jhchong92@gmail.com>
2020-03-14 23:07:00 +0800

    Complete general function and layout

--- src/app/ToDo/ToDo.js
+++ src/app/ToDo/ToDo.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { Container, Input, makeStyles, TextField, InputAdornment, Icon, IconButton, Divider, Grid, Typography, Box, List, ListItem, ListItemIcon, Checkbox, ListItemText, ListItemSecondaryAction } from '@material-ui/core'
+import { Container, Input, makeStyles, TextField, InputAdornment, Icon, IconButton, Divider, Grid, Typography, Box, List, ListItem, ListItemIcon, Checkbox, ListItemText, ListItemSecondaryAction, Paper, Chip, Button, Link } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -12,11 +12,31 @@ const useStyles = makeStyles(theme => ({
     fontSize: '30px',
     marginBottom: theme.spacing(4)
   },
-  todoList: {
+  listRoot: {
     width: '100%'
   },
   todoListText: {
     fontSize: '22px'
+  },
+  listItem: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+  completedTask: {
+    textDecoration: 'line-through',
+    textColor: 'red'
+  },
+  filterStatusRoot: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1)
+    },
+    marginTop: theme.spacing(2)
+  },
+  chip: {
+    minWidth: '50px'
   }
 }))
 
@@ -29,12 +49,37 @@ export default function ToDo() {
   const [taskName, setTaskName] = useState('')
   const [todoList, setTodoList] = useState([todo])
   const [filterStatus, setFilterStatus] = useState(0)
+  const [hoverId, setHoverId] = useState(0)
   const classes = useStyles()
   
-  function handleComplete(id) {
+  const filterStatuses = () => {
+    return [
+      {
+        status: 0,
+        label: 'All',
+      },
+      {
+        status: 2,
+        label: 'Completed',
+      },
+      {
+        status: 3,
+        label: 'Deleted',
+      }
+    ]
+  }
+  const getPendingItemsHint = (count) => {
+    if (count === 1) {
+      return '1 item left'
+    }else if (count > 1) {
+      return `${count} items left`
+    }
+    return ''
+  }
+  function handleToggle(id) {
     const newList = todoList.map((item) => {
       if (item.id === id) {
-        item.status = 2
+        item.status = item.status === 1 ? 2 : 1
       }
       return item
     })
@@ -71,13 +116,22 @@ export default function ToDo() {
     ])
     setTaskName('')
   }
-  
+
+  function handleFilterStatusClick(status) {
+    setFilterStatus(status)
+  }
+
+  function clearCompleted() {
+    setTodoList(todoList.filter((item) => item.status !== 2)) 
+  }
+
+  const pendingCount = todoList.filter((item) => item.status === 1).length
+
   const finalList = filterStatus === 0 ? todoList : todoList.filter((item) => item.status === filterStatus)
 
   return (
     <Container maxWidth='sm' >
       <div className={classes.paper}>
-        
         <Input 
           className={classes.todoInput}
           fullWidth
@@ -94,41 +148,82 @@ export default function ToDo() {
           onChange={(e) => setTaskName(e.target.value)}
           onKeyDown={handleKeyDown}
           />
-          <div style={{ width: '100%' }}>
-            <Box p={1} m={1} display="flex">
-              <Box flexGrow={1}>3 items left</Box>
-              <Box >Clear completed</Box>
-            </Box>
-          </div>
-          <List className={classes.todoList}>
-            {
-              finalList.map(item => {
-                const labelId = `label-${item.id}`
-                return (
-                  <ListItem key={item.id} role={undefined} dense button onClick={() => handleComplete(item.id)}>
-                    <ListItemIcon>
-                      <Checkbox
-                        edge="start"
-                        checked={item.status === 2}
-                        tabIndex={-1}
-                        disableRipple
-                        inputProps={{ 'aria-labelledby': labelId}}
-                      />
-                    </ListItemIcon>
-                    <ListItemText id={labelId} className={classes.todoListText} primary={
-                      <Typography variant="h5">{item.name}</Typography>
-                    } />
-                    <ListItemSecondaryAction>
-                      <IconButton edge="end" aria-label="comments"
-                        onClick={() => handleDelete(item.id)}>
-                        <Icon>delete</Icon>
-                      </IconButton>
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                );
-              })
-            }
-          </List>
+          {
+            todoList.length > 0 &&
+            <div style={{ width: '100%' }}>
+              <Container>
+                <Box p={1} m={1} display="flex">
+                  <Box flexGrow={1}>{pendingCount > 0 ?  getPendingItemsHint(pendingCount) : ''}</Box>
+                    
+                      <Box>
+                        {
+                          todoList.filter((item) => item.status === 2).length > 0 && (
+                            <Link component="button" color="secondary" onClick={clearCompleted}>Clear completed</Link>
+                          )
+                        } 
+                      </Box>
+                </Box>
+
+                <Paper className={classes.listRoot} elevation={1}>
+                  
+                  <List 
+                    onMouseLeave={() => setHoverId(0)}>
+                    {
+                      finalList.map((item, index) => {
+                        const labelId = `label-${item.id}`
+                        return (
+                          <ListItem key={item.id} button 
+                            divider={index !== todoList.length -1}
+                            onMouseEnter={() => setHoverId(item.id)}
+                            
+                            onClick={() => handleToggle(item.id)}>
+                            <ListItemIcon>
+                              <Checkbox
+                                edge="start"
+                                checked={item.status === 2}
+                                tabIndex={-1}
+                                disableRipple
+                                inputProps={{ 'aria-labelledby': labelId}}
+                              />
+                            </ListItemIcon>
+                            <ListItemText id={labelId} className={classes.todoListText} primary={
+                              <Typography color={item.status === 2 ? 'textSecondary' : 'textPrimary'} className={item.status === 2 ? classes.completedTask : '' } variant="h5">{item.name}</Typography>
+                            } />
+                            {
+                              hoverId === item.id && (
+                                <ListItemSecondaryAction>
+                                  <IconButton edge="end" aria-label="comments"
+                                    onClick={() => handleDelete(item.id)}>
+                                    <Icon>delete</Icon>
+                                  </IconButton>
+                                </ListItemSecondaryAction>
+                              )
+                            }
+                          
+                          </ListItem>
+                          
+                        );
+                      })
+                    }
+                  </List>
+                </Paper>
+              </Container>
+              <div className={classes.filterStatusRoot}>
+                  {
+                    filterStatuses().map(item => {
+                      return (
+                        <Chip 
+                          key={item.status}
+                          className={classes.chip} 
+                          color={filterStatus === item.status ? 'primary' : 'default'} 
+                          label={item.label} 
+                          onClick={() => handleFilterStatusClick(item.status)}/>
+                      )
+                    })
+                  }
+              </div>
+            </div>
+          }
       </div>
     </Container>
   )
