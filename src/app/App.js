8baa34cb74aec8b100a891d6cf58b5e031a8b341
Jeunn Hao Chong <jhchong92@gmail.com>
2020-03-14 13:05:51 +0800

    Create log in page

--- src/app/App.js
+++ src/app/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 // import logo from '../assets/logo.svg';
 import './App.css';
 import SignUp from './Auth/SignUp';
+import LogIn from './Auth/LogIn';
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      <LogIn />
+      {/* <SignUp /> */}
     </div>
   );
 }
