45fb17f99860d63134087b4f45076ff3ee9c60e0
Jeunn Hao Chong <jhchong92@gmail.com>
2020-03-14 19:11:54 +0800

    Add task with button

--- src/app/ToDo/ToDo.js
+++ src/app/ToDo/ToDo.js
@@ -45,20 +45,31 @@ export default function ToDo() {
     if (event.which === 13) { // enter pressed
       console.log('enter pressed')
       // add a todo
-      if (taskName && taskName !== '') {
-        const timestamp = (new Date()).getTime();
-        setTodoList([
-          ...todoList,
-          {
-            id: timestamp,
-            name: taskName,
-            status: 1
-          }
-        ])
-      }
+      addTask()
     }else if (event.which === 27) { // escape pressed
-
+      setTaskName('')
     }
+  }
+
+  function handleDelete(id) {
+    const list = todoList.filter((item) => item.id !== id)
+    setTodoList(list)
+  }
+
+  function addTask() {
+    if (taskName === '') {
+      return
+    }
+    const timestamp = (new Date()).getTime();
+    setTodoList([
+      ...todoList,
+      {
+        id: timestamp,
+        name: taskName,
+        status: 1
+      }
+    ])
+    setTaskName('')
   }
   
   const finalList = filterStatus === 0 ? todoList : todoList.filter((item) => item.status === filterStatus)
@@ -72,8 +83,11 @@ export default function ToDo() {
           fullWidth
           placeholder="What is your task?"
           endAdornment={
+            taskName !== '' && 
             <InputAdornment position="end">
-              <IconButton fontSize="large"><Icon>label</Icon></IconButton>
+              <IconButton fontSize="large"
+                onClick={() => addTask()}
+              ><Icon>label</Icon></IconButton>
             </InputAdornment>
           }
           value={taskName}
@@ -105,7 +119,8 @@ export default function ToDo() {
                       <Typography variant="h5">{item.name}</Typography>
                     } />
                     <ListItemSecondaryAction>
-                      <IconButton edge="end" aria-label="comments">
+                      <IconButton edge="end" aria-label="comments"
+                        onClick={() => handleDelete(item.id)}>
                         <Icon>delete</Icon>
                       </IconButton>
                     </ListItemSecondaryAction>
